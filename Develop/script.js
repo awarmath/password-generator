e85ceba518b0e6684ed8c1adda767a29de918763
awarmath <andrew@carmunity.io>
2020-10-18 22:54:15 -0500

    add all scenarios

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,102 @@
 // Assignment code here
+// define all character types
+lower = ["a, b, c, d"];
+upper = ["A, B, C, D"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+character = ["!@#$%^&*()[]{}<>,./?"];
+
+var enter;
+var confirmUppercase;
+var confirmLowercase;
+var confirmNumber;
+var confirmCharacter;
 
 function generatePassword() {
-  // Asks for user input
+  // begin prompts for number of characters
   enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-  // First if statement for user validation 
   if (!enter) {
       alert("This needs a value");
   } else if (enter < 8 || enter > 128) {
-      // Validates user input
-      // Start user input prompts
+      // Alert if user chooses anything outside of 8 and 128
       enter = parseInt(prompt("You must choose between 8 and 128"));
 
   } else {
-      // Continues once user input is validated
-      confirmNumber = confirm("Will this contain numbers?");
-      confirmCharacter = confirm("Will this contain special characters?");
-      confirmUppercase = confirm("Will this contain Uppercase letters?");
-      confirmLowercase = confirm("Will this contain Lowercase letters?");
+      // Prompts for lowercase, uppercase, number and characters
+      confirmLowercase = confirm("Would you like Lowercase letters?");
+      confirmUppercase = confirm("Would you like Uppercase letters?");
+      confirmNumber = confirm("Would you like Numbers?");
+      confirmCharacter = confirm("Would you like Special Characters?");
   };
+      // Error message if 4 cancels
+      if (!confirmLowercase && !confirmUppercase && !confirmNumber && !confirmCharacter) {
+        choices = alert("You must choose a criteria!");
+
+    }
+    // If all 4 options are chosen
+    else if (confirmLowercase && confirmUppercase && confirmNumber && confirmCharacter) {
+
+        choices = lower.concat(upper, number, character);
+    }
+    // If 3 options are chosen
+    else if (confirmLowercase && confirmUppercase && confirmNumber) {
+        choices = lower.concat(upper, number);
+    }
+    else if (confirmLowercase && confirmUppercase && confirmCharacter) {
+        choices = lower.concat(upper, character);
+    }
+    else if (confirmLowercase && confirmNumber && confirmCharacter) {
+        choices = lower.concat(number, character);
+    }
+    else if (confirmUppercase && confirmNumber && confirmCharacter) {
+        choices = upper.concat(number, character);
+    }
+    // If 2 options are chosen
+    else if (confirmLowercase && confirmUppercase) {
+        choices = lower.concat(upper);
+
+    } else if (confirmLowercase && confirmNumber) {
+        choices = lower.concat(number);
+
+    } else if (confirmLowercase && confirmCharacter) {
+        choices = lower.concat(character);
+    }
+    else if (confirmUppercase && confirmNumber) {
+        choices = upper.concat(number);
+
+    } else if (confirmUppercase && confirmCharacter) {
+        choices = upper.concat(character);
+
+    } else if (confirmNumber && confirmCharacter) {
+        choices = number.concat(character);
+    }
+    // If 1 option is chosen
+    else if (confirmLowercase) {
+        choices = lower;
+    }
+    else if (confirmUppercase) {
+        choices = upper;
+    }
+    else if (confirmNumber) {
+        choices = number;
+    }
+    else if (confirmCharacter) {
+      choices = character;
+  
+    };
+
 }
+
+
+// Place password into textbox
+function UserInput(ps) {
+  document.getElementById("password").textContent = ps;
+
+}
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
