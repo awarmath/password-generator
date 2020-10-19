006511782541be030c4fc31efe7e87d7ac8d7f56
awarmath <andrew@carmunity.io>
2020-10-18 21:29:03 -0500

    add letters, numbers and Symbols

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,26 @@
 // Assignment code here
+function genRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+function genRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+}
+function genRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+function genRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+console.log(genRandomLower());
+console.log(genRandomUpper());
+console.log(genRandomNumber());
+console.log(genRandomSymbol());
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+  
 
 // Write password to the #password input
 function writePassword() {
